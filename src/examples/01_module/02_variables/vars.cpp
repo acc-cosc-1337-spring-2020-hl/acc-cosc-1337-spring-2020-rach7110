f159a0ef0629413c43356772d9433e59a3378acf
Rachel Loziuk <rachel.loziuk@gmail.com>
2020-01-27 19:10:55 -0600

    create a square_number function; adda test case.

--- src/examples/01_module/02_variables/vars.cpp
+++ src/examples/01_module/02_variables/vars.cpp
@@ -1,1 +1,12 @@
-//cpp
+#include "vars.h"
+
+
+void display_number(int blah)
+{
+	cout << blah << endl;
+}
+
+int square_number(int num)
+{
+	return num * num;
+}
--- src/examples/01_module/02_variables/vars.h
+++ src/examples/01_module/02_variables/vars.h
@@ -1,1 +1,8 @@
 //header
+#include<iostream>
+
+using std::cout;
+using std::endl;
+
+void display_number(int num1);
+int square_number(int num1);
