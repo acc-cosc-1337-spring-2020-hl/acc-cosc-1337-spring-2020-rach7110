3d7dc7f3e9c02bed6ec1bb52d7452490d3b80f66
Rachel Loziuk <rachel.loziuk@gmail.com>
2020-01-29 18:48:48 -0600

    created a function and unit test for multiplying numbers.

--- src/homework/01_expressions/main.cpp
+++ src/homework/01_expressions/main.cpp
@@ -1,15 +1,18 @@
 ﻿//write include statements
-
+#include<iostream>;
+#include "expressions.h";
 
 //write namespace using statement for cout
-
+using namespace std;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	int result = multiply_numbers(5,5);
+
+	cout << result;
 
 	return 0;
 }
--- test/homework_test/01_expressions_test/01_expressions_tests.cpp
+++ test/homework_test/01_expressions_test/01_expressions_tests.cpp
@@ -12,3 +12,11 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE("Verify multiplication numbers function")
+{
+	REQUIRE(multiply_numbers(5, 5) == 25);
+	REQUIRE(multiply_numbers(5, 5) != 5*5);
+	REQUIRE(multiply_numbers(2, 0) == 0);
+	REQUIRE(multiply_numbers(2, -5) == -10);
+}
+
