5a6599ca3e7c2ebbd2693dee4a584b435d689426
Rachel Loziuk <rachel.loziuk@gmail.com>
2020-02-10 18:58:38 -0600

    create functions for loops.

--- src/examples/03_module/03_do_while/do_while.cpp
+++ src/examples/03_module/03_do_while/do_while.cpp
@@ -1,12 +1,30 @@
 #include<iostream>
 #include "do_while.h"
 #include "switch.h"
+#include "while.h"
 
 using std::cout;
 using std::cin;
 
 //Write code for void function prompt_user to loop until
-//user opts not to continue.  
+//user opts not to continue.
+void prompt_user()
+{
+	int choice;
+
+	do
+	{
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+
+		cout << sum_of_squares(num) << "\n";
+
+		cout << "Hello...continue?";
+		cin >> choice;
+	} while (choice);
+
+}
 
 
 //Write code for function run_menu that prompts  user for a 
