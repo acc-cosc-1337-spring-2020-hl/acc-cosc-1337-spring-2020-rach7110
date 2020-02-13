16774d88d1709a56e2bda0bb2701759d3d6992d3
Rachel <rachel.loziuk@gmail.com>
2020-02-12 18:56:17 -0600

    cw 3

--- src/classwork/03_assign/loops.h
+++ src/classwork/03_assign/loops.h
@@ -1,2 +1,6 @@
 //Write prototype for function factorial that accepts an int num
 //and returns an int
+
+// write function prototype for a value - return function named factorial that accepts 
+// an int parameter named num and  returns an int.
+int factorial(int num);
