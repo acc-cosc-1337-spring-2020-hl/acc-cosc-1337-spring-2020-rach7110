77f52602d0a34b316f38905872991f4eb9240c28
Rachel Loziuk <rachel.loziuk@gmail.com>
2020-02-17 18:54:10 -0600

    create functions that work with vectors, for range loops, and pass by value/reference parameters.

--- src/examples/03_module/06_vectors/vec.cpp
+++ src/examples/03_module/06_vectors/vec.cpp
@@ -1,8 +1,8 @@
-#include "for_ranged.h"
 #include<iostream>
 #include<vector>
+#include "vec.h";
 
-using std::vector;
+using std::cout;
 
 /*
 Write code for function loop_vector_w_index with no parameters.
@@ -17,15 +17,28 @@ Write code for function loop_vector_w_index with a vector of int pass by value p
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
 */
-
-
+void loop_vector_w_index(vector<int> nums)
+{
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		cout << nums[i] << "\n";
+	}
+}
 
 /*
 Write code for function loop_vector_w_index with a vector of int pass by reference parameter.
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows : 9 10 99 5  67
 */
+void loop_vector_w_index_ref(vector<int>& nums)
+{
+	for (auto &n : nums)  // use 'auto' because it's simplier. Don't need to declare an iterator.
+	{
+		n = 1;
 
+		cout << n << "\n";
+	}
+}
 
 /*
 Write code for function loop_vector_w_index with a vector of int const pass by reference parameter.
