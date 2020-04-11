3fb76b9ef64ed46af2e57f4a25e6dc748628bf75
Rachel Loziuk <rachel@reviewpush.com>
2020-04-11 18:40:10 -0500

    HW08: implment function in TTTManager

--- src/homework/tic_tac_toe/tic_tac_toe_manager.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.cpp
@@ -1,1 +1,36 @@
-//cpp
+#include "tic_tac_toe_manager.h"
+
+
+void TicTacToeManager::save_game(TicTacToe game)
+{
+    games.push_back(game);
+    
+    update_winner_count(game.get_winner());
+}
+
+// TODO: is this method necessary?
+void TicTacToeManager::get_winner_totals(int &o, int &x, int &t)
+{
+    
+}
+
+void TicTacToeManager::update_winner_count(string winner)
+{
+    if (winner == "X") {
+        ++x_wins;
+    } else if (winner == "O") {
+        ++o_wins;
+    } else if (winner == "C") {
+        ++ties;
+    }
+        
+}
+
+ostream &operator<<(ostream &out, const TicTacToeManager &manager)
+{
+    for(auto game : manager.games) {
+        cout << game;
+        cout << "Winner: " << game.get_winner();
+        
+    }
+}
--- src/homework/tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.h
@@ -7,14 +7,14 @@ class TicTacToeManager
 {
 public:
     void save_game(TicTacToe game);
-    friend ostream &operator<<(ostream &out, TicTacToeManager manager);
+    friend ostream &operator<<(ostream &out, const TicTacToeManager &manager);
     void get_winner_totals(int &o, int &x, int &t);  // TODO: is this necessary?
     
 private:
     vector<TicTacToe> games;
-    int o_win = 0;
+    int o_wins = 0;
     int x_wins = 0;
-    int tie = 0;
+    int ties = 0;
     void update_winner_count(string winner);
 };
 
