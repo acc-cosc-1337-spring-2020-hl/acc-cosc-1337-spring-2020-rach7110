a07330f4759bfd2868759bc3f5e9e384b084698b
Rachel Loziuk <rachel@reviewpush.com>
2020-04-11 17:58:12 -0500

    HW08: write headers for  TTTManager class

--- src/homework/tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,22 @@
-//h
+#ifndef TIC_TAC_TOE_MANAGER_H
+#define TIC_TAC_TOE_MANAGER_H
+
+#include "tic_tac_toe.h"
+
+class TicTacToeManager
+{
+public:
+    void save_game(TicTacToe game);
+    friend ostream &operator<<(ostream &out, TicTacToeManager manager);
+    void get_winner_totals(int &o, int &x, int &t);  // TODO: is this necessary?
+    
+private:
+    vector<TicTacToe> games;
+    int o_win = 0;
+    int x_wins = 0;
+    int tie = 0;
+    void update_winner_count(string winner);
+};
+
+
+#endif
