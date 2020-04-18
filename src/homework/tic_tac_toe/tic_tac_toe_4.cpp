53d26b05a4ecb94462f99bfa077de24262ab29f6
Rachel Loziuk <rachel@reviewpush.com>
2020-04-18 17:06:26 -0500

    HW09: create check win methods for TTT4

--- src/homework/tic_tac_toe/tic_tac_toe_4.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_4.cpp
@@ -11,7 +11,33 @@ else
 false
 */
 
+bool TicTacToe4::check_column_win() {
+    if ((pegs[0] == "X" && pegs[4] == "X" && pegs[8] == "X" && pegs[12] == "X")  || (pegs[0] == "O" && pegs[4] == "O" && pegs[8] == "O" && pegs[12] == "O") ) {
+        set_winner();
+        
+        return true;
+    }
+    
+    if ((pegs[1] == "X" && pegs[5] == "X" && pegs[9] == "X" && pegs[13] == "X")  || (pegs[1] == "O" && pegs[5] == "O" && pegs[9] == "O" && pegs[13] == "0") ) {
+        set_winner();
+        
+        return true;
+    }
+    
+    if ((pegs[2] == "X" && pegs[6] == "X" && pegs[10] == "X" && pegs[14] == "X")  || (pegs[2] == "O" && pegs[6] == "O" && pegs[10] == "O" && pegs[14] == "0") ) {
+        set_winner();
+        
+        return true;
+    }
+    
+    if ((pegs[3] == "X" && pegs[7] == "X" && pegs[11] == "X" && pegs[15] == "X")  || (pegs[3] == "O" && pegs[7] == "O" && pegs[11] == "O" && pegs[15] == "0") ) {
+         set_winner();
+         
+         return true;
+     }
 
+    return false;
+}
 
 
 /*
@@ -22,6 +48,33 @@ Win by row if
 8, 9, 10, 11 are equal 
 12,13,14, 15 are equal
 */
+bool TicTacToe4::check_row_win() {
+    if ((pegs[0] == "X" && pegs[1] == "X" && pegs[2] == "X" && pegs[3] == "X")  || (pegs[0] == "O" && pegs[1] == "O" && pegs[2] == "O" && pegs[3] == "O") ) {
+        set_winner();
+        
+        return true;
+    }
+    
+    if ((pegs[4] == "X" && pegs[5] == "X" && pegs[6] == "X" && pegs[7] == "X")  || (pegs[4] == "O" && pegs[5] == "O" && pegs[6] == "O" && pegs[7] == "0") ) {
+        set_winner();
+        
+        return true;
+    }
+    
+    if ((pegs[8] == "X" && pegs[9] == "X" && pegs[10] == "X" && pegs[11] == "X")  || (pegs[8] == "O" && pegs[9] == "O" && pegs[10] == "O" && pegs[11] == "0") ) {
+        set_winner();
+        
+        return true;
+    }
+    
+    if ((pegs[12] == "X" && pegs[13] == "X" && pegs[14] == "X" && pegs[15] == "X")  || (pegs[12] == "O" && pegs[13] == "O" && pegs[14] == "O" && pegs[15] == "0") ) {
+         set_winner();
+         
+         return true;
+     }
+
+    return false;
+}
 
 
 
@@ -34,3 +87,20 @@ Win diagonally
 12,13,14, 15
 
 */
+bool TicTacToe4::check_diagonal_win() {
+    if ((pegs[0] == "X" && pegs[5] == "X" && pegs[10] == "X")  && pegs[15] == "X")  || (pegs[0] == "O" && pegs[5] == "O" && pegs[10] == "O"  && pegs[15] == "O")) ) {
+         
+        set_winner();
+        
+         return true;
+     }
+     
+        if ((pegs[3] == "X" && pegs[6] == "X" && pegs[9] == "X")  && pegs[12] == "X")  || (pegs[3] == "O" && pegs[6] == "O" && pegs[9] == "O"  && pegs[15] == "O")) ) {
+         
+         set_winner();
+         
+         return true;
+     }
+
+     return false;
+}
