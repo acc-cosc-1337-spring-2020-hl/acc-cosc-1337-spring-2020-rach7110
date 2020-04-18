148fd73cb696e8377a446050834da00595967c69
Rachel Loziuk <rachel@reviewpush.com>
2020-04-18 15:25:53 -0500

    Example 09 attempt

--- src/examples/09_module/temperature.h
+++ src/examples/09_module/temperature.h
@@ -1,1 +1,18 @@
 //temperature.h
+#ifndef TEMPERATURE_H
+#define TEMPERATURE_H
+
+//function code
+class Temperature
+{
+public:
+    Temperature(int h, int r) : hour(h), reading(r) {}
+    int get_reading() const{return reading; };
+    int get_hour() const { return hour;};
+
+private:
+    int hour;
+    double reading;
+};
+
+#endif
--- src/examples/09_module/temperature_data.h
+++ src/examples/09_module/temperature_data.h
@@ -1,1 +1,20 @@
 //temperature_data.h
+#include "temperature.h"
+#ifndef TEMPERATURE_DATA_H
+#define TEMPERATURE_DATA_H
+
+#include<vector>
+#include<string>
+
+class TemperatureData
+{
+public:
+    void save_temps(std::vector<Temperature>& ts);
+    std:vector<Temperature> get_temps()const;
+    
+private:
+    std::vector<Temperature> temps;
+    const std::string file_name{"temperature.dat"};
+};
+
+#endif
