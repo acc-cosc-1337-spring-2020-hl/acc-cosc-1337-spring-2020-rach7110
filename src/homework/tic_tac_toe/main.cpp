e0d2e0ab2989595e256d480f0e9440303fa40b8d
Rachel Loziuk <rachel@reviewpush.com>
2020-04-19 17:34:57 -0500

    HW09 - modify main.cpp for new game flow

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,4 +1,6 @@
 #include "tic_tac_toe.h"
+#include "tic_tac_toe_3.h"
+#include "tic_tac_toe_4.h"
 #include "tic_tac_toe_manager.h"
 
 using std::cout;
@@ -7,37 +9,56 @@ using std::cin;
 int main() 
 {
     TicTacToeManager manager;
+    TicTacToe3 game_3;
+    TicTacToe4 game_4;
+    vector<reference_wrapper<TicTacToe> >games;
     bool playing = true;
+    int size;
 
     do {
-        TicTacToe game;
+       
         string first_player;
         
-        do {
-            try {
-                cout << "Enter first player (X or O): ";
+        cout << "Choose TicTacToe3 or TicTacToe4 (3 or 4): \n";
+        cin >> size;
+        
+        if (size == 3) {
+            games.push_back(game_3);
+            manager.games.push_back(games.front());
+        }
+        
+        if (size == 4) {
+            games.push_back(game_4);
+            manager.games.push_back(games.back());
+        }
+        
+        cout << "Enter first player (X or O): ";
+        cin >> first_player;
+        
+        // Invalid first player.
+        try {
+            manager.games.back().get().start_game(first_player);
+        }
+        catch (GameError e) {
+            cout << e.get_message() << "\n";
+        }
+        
+        TicTacToe game = manager.games.back();
 
-                cin >> first_player;
-                
-                game.start_game(first_player);
-            }
-            catch (GameError e) {
-                cout << e.get_message() << "\n";
-            }
-        } while (game.get_player() == "");
-
+        // Play game.
         do {
             cin >> game;
-
             cout << game;
 
         } while (!game.game_over());
         
+        // Game is over, so output results and ask if user wants to play again.
         if (game.game_over()) {
             cout << "Game over! \n";
             manager.save_game(game);
             cout << manager;
             manager.get_winner_totals();
+            
             cout << "Play again? (Enter 1-Yes, 0-No) \n";
             cin >> playing;
         }
