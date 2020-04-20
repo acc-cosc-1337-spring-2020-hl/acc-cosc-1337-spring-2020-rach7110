052d7985e0b93dc15062f4725e3418c7f77f4e1b
Rachel Loziuk <rachel@reviewpush.com>
2020-04-20 18:02:57 -0500

    WIP: Dangling pointers

--- src/examples/11_module/04_dangling_pointer/main.cpp
+++ src/examples/11_module/04_dangling_pointer/main.cpp
@@ -1,8 +1,16 @@
+#include<iostream>
 #include "dangling_pointer.h"
 
 int main() 
 {
-	
-
+    int *ptr_num = new int(5);
+    int *ptr = ptr_num;
+    std::cout << *ptr << "\n";
+    
+    delete ptr_num;
+    std::cout << *ptr;
+    
+    ptr_num = nullptr;
+    
 	return 0;
 }
